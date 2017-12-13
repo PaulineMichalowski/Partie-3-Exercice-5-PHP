e2cde387e24b762ca461d8e73897ca44c391af01
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:14:53 +0100

    Création de l'exercice 5

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <?php
+      for($line_number = 1; $line_number <= 15; $line_number++) {
+        echo 'On y arrive presque ! ';
+      }
+    ?>
+  </body>
+</html>
